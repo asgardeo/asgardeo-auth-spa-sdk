b539c2177740b2fd4ecc40b12556eb65d352578a
Thivi <theviyanthan@outlook.com>
2021-02-07 02:58:11 +0530

    Add sign in model

--- lib/src/models/sign-in.ts
+++ lib/src/models/sign-in.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -16,10 +16,6 @@
  * under the License.
  */
 
-export * from "./client";
-export * from "./message";
-export * from "./http-client";
-export * from "./web-worker";
-export * from "./session-management-helper";
-export * from "./client-config";
-export * from "./sign-in";
+import { GetAuthURLConfig } from "@asgardeo/auth-js";
+
+export type SignInConfig = GetAuthURLConfig & { callOnlyOnRedirect: boolean; };
